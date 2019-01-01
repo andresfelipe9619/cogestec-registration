ca09ed2de79a1c1984d6fc331adfbccc0a968f6e
Andres Suarez <andresfelipe9619@gmail.com>
2019-01-01 14:47:17 -0500

    Refactor email service and change some logic in main

--- back/emailService.js
+++ back/emailService.js
@@ -1,3 +1,4 @@
+var SELECTED_PERSON = { data: null, type: null, isApproved: false };
 function onSpreadSheetEdit(e) {
   var range = e.range;
   Logger.log(range);
@@ -6,134 +7,170 @@ function onSpreadSheetEdit(e) {
 
 function checkEditedCell(range) {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  var rawPerson = [];
-  var payload = { person: null, type: null };
-  //SI PAGO COMPROBADO
-  if (range.getColumn() == 8 && range.getValue() == "SI") {
-    var sheetValues = sheet.getSheetValues(
-      range.getRow(),
-      1,
-      1,
-      sheet.getLastColumn()
-    );
-    Logger.log(sheetValues);
-    rawPerson = sheetValues[0];
-    payload.type = "PAY";
-    payload.person = {
-      cedula: rawPerson[2],
-      nombre: rawPerson[0] + " " + rawPerson[1],
-      email: rawPerson[3]
-    };
+  var sheetValues = sheet.getSheetValues(
+    range.getRow(),
+    1,
+    1,
+    sheet.getLastColumn()
+  );
+  Logger.log(sheetValues);
+  var rawPerson = sheetValues[0];
+  rawPerson = rawPerson.map(function(value){
+    return value.toString();
+  })
+  SELECTED_PERSON.data = {
+    cedula: rawPerson[2],
+    nombre: rawPerson[0] + " " + rawPerson[1],
+    email: rawPerson[3],
+    pago_total: rawPerson[6],
+    concepto_pago: rawPerson[5],
+    dependecia: "COGESTEC 2019",
+    telefono: rawPerson[4]
+  };
+  if (range.getColumn() == 8) {
+    handleOnPaymentChange(range);
+  } else if (range.getColumn() == 10) {
+    handleOnDocumentChange(range);
   }
-  //SI DOCUMENTO VERIFICADO
-  if (range.getColumn() == 10 && range.getValue() == "SI") {
-    var sheetValues = sheet.getSheetValues(
-      range.getRow(),
-      1,
-      1,
-      sheet.getLastColumn()
-    );
-    Logger.log(sheetValues);
-    rawPerson = sheetValues[0];
-    payload.type = "DOC";
-    payload.person = {
-      cedula: rawPerson[2],
-      nombre: rawPerson[0] + " " + rawPerson[1],
-      email: rawPerson[3],
-      pago_total: rawPerson[6],
-      concepto_pago: rawPerson[5],
-      dependecia: "COGESTEC 2019",
-      telefono: rawPerson[4]
-    };
-  }
-
-  Logger.log(payload);
-  sendConfirmationEmail(payload);
+  Logger.log(SELECTED_PERSON);
 }
 
-function sendConfirmationEmail(payload) {
-  var body = "";
-  if (payload) {
-    body = createHtmlBody(payload);
-    if (payload.type == "DOC") {
-      MailApp.sendEmail({
-        to: payload.person.email,
-        subject: "Inscripción verificada",
-        name: "COGESTEC 2019",
-        // attachments: [],
-        htmlBody: body
-      });
-    } else if (payload.type == "PAY") {
-      MailApp.sendEmail({
-        to: payload.person.email,
-        subject: "Pago comprobado",
-        name: "COGESTEC 2019",
-        htmlBody: body
-      });
+function handleOnPaymentChange(range) {
+  SELECTED_PERSON.type = "PAY";
+  Logger.log(typeof SELECTED_PERSON.data.concepto_pago);
+  Logger.log(SELECTED_PERSON.data.concepto_pago)
+  if (range.getValue() == "SI") {
+    if (SELECTED_PERSON.data.concepto_pago.indexOf("Researcher") !== -1) {
+      sendResearcherPayApprovedMail();
+    } else if (SELECTED_PERSON.data.concepto_pago.indexOf("Attendant") !== -1) {
+      sendAttendantPayApprovedMail();
     }
+  } else if (range.getValue() == "NO") {
+    sendPayDisapprovedMail();
   }
 }
 
-function createHtmlBody(payload) {
-  var result = "";
-  var person = payload.person;
-  if (payload.type == "DOC") {
-    result = generateModal(person);
-  } else if (payload.type == "PAY") {
-    result = generateQR(person);
+function handleOnDocumentChange(range) {
+  SELECTED_PERSON.type = "DOC";
+  if (range.getValue() == "SI") {
+    sendDosendcApprovedMail();
+  } else if (range.getValue() == "NO") {
+    sendDocDisapprovedMail();
   }
-  return result;
 }
 
-function generateQR(person) {
+function sendDocApprovedMail() {
+  var htmlBody = buildDocApprovedBody();
+  var subject = "Solicitud de descuento COGESTEC Aceptado.";
+  sendEmail(subject, htmlBody);
+}
+
+function sendDocDisapprovedMail() {
+  var htmlBody = buildDocDisapprovedBody();
+  var subject = "Solicitud de descuento COGESTEC Denegada.";
+  sendEmail(subject, htmlBody);
+}
+
+function sendAttendantPayApprovedMail() {
+  var htmlBody = buildAttendantPayApprovedBody();
+  var subject = "Confirmación de pago Asistente";
+  sendEmail(subject, htmlBody);
+}
+
+function sendResearcherPayApprovedMail() {
+  var htmlBody = buildResearcherPayApprovedBody();
+  var subject = "Confirmación de pago Ponente";
+  sendEmail(subject, htmlBody);
+}
+
+function sendPayDisapprovedMail() {}
+
+function sendEmail(subject, body) {
+  Logger.log("I like the way you french inhale");
   var body = "";
-  var qrserver = "http://api.qrserver.com/v1/create-qr-code/";
-  var successMsg = "<p>Cordial saludo "+person.nombres + " "+ person.apellidos +
-  ", bienvenido a COGESTEC 2019 el evento más innovador de Colombia, en este email adjuntaremos un código QR, por favor,"+
-  " presentalo en el ingreso al evento académico para que hagas el ingreso en las sedes del evento.</p>";
-  if(person.concepto_pago.contains("esearcher")){
-    successMsg += "Cordial saludo (nombre y apellidos), bienvenido a COGESTEC 2019 el evento más innovador de Colombia, en este email adjuntaremos un código QR, por favor, presentalo en el ingreso al evento académico para que hagas el ingreso en las sedes del evento."+
-    "(adicionar banner publicitario del evento académico)"+
-    "Podrás consultar la agenda en el siguiente enlace: http://www.cogestec.co/agenda/"+
-    "Recuerda que obtendrás certificado al pasar por las mesas de registro del evento académico."
-  }else {
-    successMsg += 
-    "<p>Recuerda que obtendrás certificado como ponente por presentar tu ponencia en la fecha y hora asignadas y como asistente al evento, al pasar por la mesa de registro el día del evento."+
-    "Recuerda que siempre podrás consultar la agenda en el siguiente enlace: http://www.cogestec.co/agenda/"+
-    "La información de interés para ponentes se encuentra en el siguiente enlace:"+
-    "http://www.cogestec.co/ponentes"+
-    "Te agradecemos por hacer parte de la historia de la innovación en Colombia, esperamos conozcas personas y empresas interesadas en la innovación como tú, de la que puedan construir relaciones de mutuo beneficio."+
-    "COGESTEC 2019 te desea un excelente día.</p>"
+  if (SELECTED_PERSON.data) {
+    MailApp.sendEmail({
+      to: SELECTED_PERSON.data.email,
+      subject: subject,
+      name: "COGESTEC 2019",
+      htmlBody: body
+      // attachments: [],
+    });
   }
-  body = successMsg;
+}
+
+function getPersonQR() {
+  var qrserver = "http://api.qrserver.com/v1/create-qr-code/";
   var qrimage =
-    '' +
+    "" +
     '<img src="' +
     qrserver +
     "?color=000000&amp;bgcolor=FFFFFF&amp;" +
     "data=" +
-    person.cedula +
+    SELECTED_PERSON.data.cedula +
     '&amp;qzone=1&amp;margin=0&amp;size=400x400&amp;ecc=L" alt="qr code" />';
-    successMsg += body;
   return qrimage;
 }
 
-function generateModal(person) {
+function buildResearcherPayApprovedBody() {
+  var body = "";
+  var successMsg =
+    "<p>Cordial saludo " +
+    SELECTED_PERSON.data.nombres +
+    ", bienvenido a COGESTEC 2019 el evento más innovador de Colombia, en este email adjuntaremos un código QR, por favor," +
+    " presentalo en el ingreso al evento académico para que hagas el ingreso en las sedes del evento.</p>";
+  +"Cordial saludo (nombre y apellidos), bienvenido a COGESTEC 2019 el evento más innovador de Colombia, en este email adjuntaremos un código QR, por favor, presentalo en el ingreso al evento académico para que hagas el ingreso en las sedes del evento." +
+    "(adicionar banner publicitario del evento académico)" +
+    "Podrás consultar la agenda en el siguiente enlace: http://www.cogestec.co/agenda/" +
+    "Recuerda que obtendrás certificado al pasar por las mesas de registro del evento académico.";
+  "La información de interés para ponentes se encuentra en el siguiente enlace:" +
+    "http://www.cogestec.co/ponentes" +
+    "Te agradecemos por hacer parte de la historia de la innovación en Colombia, esperamos conozcas personas y empresas interesadas en la innovación como tú, de la que puedan construir relaciones de mutuo beneficio." +
+    "COGESTEC 2019 te desea un excelente día.</p>";
+  body = successMsg;
+  var qr = getPersonQR();
+  body = body.concat(qr);
+  return body;
+}
 
+function buildAttendantPayApprovedBody() {
+  var body = "";
+  var successMsg =
+    "<p>Cordial saludo (nombre y apellidos), bienvenido a COGESTEC 2019 el evento más innovador de Colombia," +
+    " en este email adjuntaremos un código QR, por favor, presentalo en el ingreso al evento académico para que hagas el ingreso en las sedes del evento." +
+    "(adicionar banner publicitario del evento académico)" +
+    "Podrás consultar la agenda en el siguiente enlace: http://www.cogestec.co/agenda/" +
+    "Recuerda que obtendrás certificado al pasar por las mesas de registro del evento académico.</p>";
+  body = successMsg;
+  var qr = getPersonQR();
+  body = body.concat(qr);
+  return body;
+}
+
+function buildDocDisapprovedBody() {
+  var body = "";
+  var successMsg =
+    "<p>Cordial saludo (nombre y apellidos), lamentamos informar que tu solicitud de descuento ha sido denegada puesto que," +
+    "la información adjunta es insuficiente para comprobar tu estado actual como estudiante de pregrado activo." +
+    "No te preocupes, aún puedes participar como asistente." +
+    "(Adjuntar la información para pago como asistente).</p>";
+  body = successMsg;
+  return body;
+}
+
+function buildDocApprovedBody() {
+  var successMsg = "<p></p>";
   var modal =
     "<!DOCTYPE html>" +
     "<html>" +
     "<head>" +
     '<base target="_top" />' +
     '<meta http-equiv="Content-Security-Policy" content="connect-src http:" />' +
-    "<link" +
-    'rel="stylesheet"' +
-    'href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"' +
-    "/>" +
-    "<?!= include('style'); ?>" +
     " </head><body>" +
     '<div class="content">' +
-    '<div id="result-msg" class="ui message">'+successMsg +'</div>' +
+    '<div id="result-msg" class="ui message">' +
+    successMsg +
+    "</div>" +
     '<form class="ui form not-visible" id="modal-form">' +
     '<div id="pay_info_modal" ' +
     'class="ui medium center aligned inverted header">' +
@@ -146,7 +183,7 @@ function generateModal(person) {
     'class="numeric" ' +
     'name="cedula_modal" ' +
     'type="text" value="' +
-    person.cedula +
+    SELECTED_PERSON.data.cedula +
     '" />' +
     "</div>" +
     '<div class="inline field">' +
@@ -154,7 +191,7 @@ function generateModal(person) {
     "<input " +
     'name="concepto_pago_modal"' +
     'type="text" value="' +
-    person.concepto_pago +
+    SELECTED_PERSON.data.concepto_pago +
     '" />' +
     "</div>" +
     '<div class="inline field">' +
@@ -163,13 +200,13 @@ function generateModal(person) {
     'name="pago_total_modal"' +
     'class="numeric"' +
     'type="text" value="' +
-    person.pago_total +
+    SELECTED_PERSON.data.pago_total +
     '" />' +
     "</div>" +
     '<div class="inline field">' +
     "<label>*Nombre Completo</label>" +
     '<input name="nombres_modal" placeholder="Nombres" type="text" value="' +
-    person.nombre +
+    SELECTED_PERSON.data.nombre +
     '" />' +
     "</div>" +
     '<div class="inline field">' +
@@ -177,7 +214,7 @@ function generateModal(person) {
     "<input " +
     'name="dependencia_modal "' +
     'type="text" value="' +
-    person.dependecia +
+    SELECTED_PERSON.data.dependecia +
     '" />' +
     "</div>" +
     '<div class="inline field">' +
@@ -186,7 +223,7 @@ function generateModal(person) {
     'name="telefono_modal"' +
     'class="numeric "' +
     'type="text" value="' +
-    person.telefono +
+    SELECTED_PERSON.data.telefono +
     '" />' +
     "</div>" +
     "<br />" +
@@ -201,25 +238,4 @@ function generateModal(person) {
   "</a>" + "</div>" + "</form>" + "</div>" + "</body></html>";
 
   return modal;
-}
-
-function sendPostRequest(payload) {
-  var url =
-    "https://script.google.com/macros/s/AKfycbw3gbuDvJwe5_pccF52DLGu8Jqeh8zKmy-_P14omklUvu74Kb2z/exec";
-  var options = {
-    method: "POST",
-    payload: JSON.stringify(payload),
-    contentType: "application/json",
-    followRedirects: true,
-    muteHttpExceptions: true,
-    validateHttpsCertificates: false
-  };
-  Logger.log("FETCHING...");
-  var result = UrlFetchApp.fetch(url, options);
-  Logger.log(result);
-  if (result.getResponseCode() == 200) {
-    var params = JSON.parse(result.getContentText());
-    Logger.log("Email send correctly");
-    Logger.log(params);
-  }
 }
